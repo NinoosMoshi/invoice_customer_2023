e1fc78898c8ec55c5cc9454cce3b67ac05af6dd0
NinoosMoshi <nalkaton@gmail.com>
2023-07-09 08:59:12 -0500

    create components and login html

--- frontEnd/invoice-ui/src/app/app-routing.module.ts
+++ frontEnd/invoice-ui/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { VerfiyComponent } from './components/verfiy/verfiy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"resetpassword", component:ResetpasswordComponent},
+  {path:"user/verfiy/account/:key", component:VerfiyComponent},
+  {path:"user/verfiy/password/:key", component:VerfiyComponent},
+  {path:"**", component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontEnd/invoice-ui/src/app/app.module.ts
+++ frontEnd/invoice-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerfiyComponent } from './components/verfiy/verfiy.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VerfiyComponent,
+    ResetpasswordComponent
   ],
   imports: [
     BrowserModule,
